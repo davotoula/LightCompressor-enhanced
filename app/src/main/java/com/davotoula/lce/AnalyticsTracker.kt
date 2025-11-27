4dc85c29e45d91b88c81f6cc2145738abe05f18a
davotoula <david.kaspar@gmail.com>
2025-11-27 11:13:37 +0100

    upgrade firebase-bom to latest

--- app/src/main/java/com/davotoula/lce/AnalyticsTracker.kt
+++ app/src/main/java/com/davotoula/lce/AnalyticsTracker.kt
@@ -1,10 +1,10 @@
 package com.davotoula.lce
 
+import com.google.firebase.Firebase
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.analytics.ktx.logEvent
-import com.google.firebase.crashlytics.ktx.crashlytics
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.analytics.analytics
+import com.google.firebase.analytics.logEvent
+import com.google.firebase.crashlytics.crashlytics
 
 object AnalyticsTracker {
 
--- app/src/main/java/com/davotoula/lce/LceApplication.kt
+++ app/src/main/java/com/davotoula/lce/LceApplication.kt
@@ -2,9 +2,9 @@ package com.davotoula.lce
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.crashlytics.ktx.crashlytics
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.analytics
+import com.google.firebase.crashlytics.crashlytics
 
 class LceApplication : Application() {
 
