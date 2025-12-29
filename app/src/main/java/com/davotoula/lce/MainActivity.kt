6a4972a6244f6b7ade3678af5a0c4efac4c226d2
davotoula <david.kaspar@gmail.com>
2025-12-29 13:42:56 +0000

    update MainActivity to use Compose NavHost

--- app/src/main/java/com/davotoula/lce/MainActivity.kt
+++ app/src/main/java/com/davotoula/lce/MainActivity.kt
@@ -3,34 +3,21 @@ package com.davotoula.lce
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.davotoula.lce.navigation.LceNavHost
+import com.davotoula.lce.ui.theme.LceTheme
 
-/**
- * Main Activity - placeholder for Compose migration
- * TODO: Implement full Compose UI in subsequent tasks
- */
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
         setContent {
-            MaterialTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text("LightCompressor - Compose UI Coming Soon")
-                    }
-                }
+            LceTheme {
+                val navController = rememberNavController()
+                LceNavHost(navController = navController)
             }
         }
     }
