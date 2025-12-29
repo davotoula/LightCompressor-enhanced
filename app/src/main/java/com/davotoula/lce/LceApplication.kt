758cafdc2885cbcd40351ca878cb8f58f4649449
davotoula <david.kaspar@gmail.com>
2025-12-29 16:05:28 +0000

    add thumbnail extraction

--- app/src/main/java/com/davotoula/lce/LceApplication.kt
+++ app/src/main/java/com/davotoula/lce/LceApplication.kt
@@ -1,12 +1,15 @@
 package com.davotoula.lce
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.VideoFrameDecoder
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
 import com.google.firebase.analytics.analytics
 import com.google.firebase.crashlytics.crashlytics
 
-class LceApplication : Application() {
+class LceApplication : Application(), ImageLoaderFactory {
 
     override fun onCreate() {
         super.onCreate()
@@ -20,5 +23,13 @@ class LceApplication : Application() {
 
         Firebase.analytics.setAnalyticsCollectionEnabled(true)
         AnalyticsTracker.logAppOpen()
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .components {
+                add(VideoFrameDecoder.Factory())
+            }
+            .build()
     }
 }
